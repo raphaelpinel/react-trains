6d0ffd79d4a95f65633982cf963840e0501f3e3c
Raphael Pinel <raphaelpinel8@gmail.com>
2019-02-11 18:48:17 +0200

    styled display as table

--- src/components/DataDisplay/DataDisplay.js
+++ src/components/DataDisplay/DataDisplay.js
@@ -6,21 +6,29 @@ import styles from './DataDisplay.module.css'
 const DataDisplay = props => {
     let filteredData;
     props.display === 'arrival' ? filteredData = props.filteredData.filter(element => typeof(element.scheduledArrivalTime) !== 'undefined') : filteredData = props.filteredData.filter(element => typeof(element.scheduledDepartureTime) !== 'undefined');
-	console.log('TCL: filteredData', filteredData);
+    console.log('TCL: filteredData', filteredData);
+    const data = filteredData.map( train => (
+    <tr key={train.trainNumber} className="table-row">
+        <td>{train.trainNumber}</td>
+        <td>{train.origin}</td>
+        <td>{train.destination}</td>
+        <td>{(props.display === 'arrival') ? train.scheduledArrivalTime : train.scheduledDepartureTime}</td>
+    </tr>));
     
     return (
-        <div className={styles.DataDisplay}>
-            <ReactTable 
-            data={props.filteredData}
-            columns={[
-                {Header: 'Juna', accessor: 'trainNumber'},
-                {Header: 'Lähtöasema', accessor: 'origin'},
-                {Header: 'Pääteasema', accessor: 'destination'},
-                {Header: 'Saapu', accessor: (props.display === 'arrival') ? 'scheduledArrivalTime' : 'scheduledDepartureTime'}
-            ]}
-            className="-striped -highlight"
-            />
-        </div>
+        <table className={styles.DataDisplay}>
+            <thead>
+                <tr className="table-header">
+                    <th>Juna</th>
+                    <th>Lähtöasema</th>
+                    <th>Pääteasema</th>
+                    <th>Saapuu</th> 
+                </tr>
+            </thead>
+            <tbody>
+            {data}
+            </tbody>
+        </table>
     );
 }
 
