bbf68d1fb3fe018946564089fce3939612aa89a5
Raphael Pinel <raphaelpinel8@gmail.com>
2019-02-12 22:40:38 +0200

    displayed actual time in red if different from scheduled time

--- src/components/DataDisplay/DataDisplay.js
+++ src/components/DataDisplay/DataDisplay.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './DataDisplay.module.css';
+import './DataDisplay.css';
 
 const formatTime = (dateTime) => {
     return new Intl.DateTimeFormat('fi-FI', { hour: 'numeric', minute: 'numeric'}).format(new Date(dateTime.toString()));
@@ -8,18 +8,32 @@ const formatTime = (dateTime) => {
 
 const DataDisplay = props => {
     let filteredData;
-    props.display === 'arrival' ? filteredData = props.filteredData.filter(element => typeof(element.scheduledArrivalTime) !== 'undefined').sort((a, b) => (a.scheduledArrivalTime > b.scheduledArrivalTime) ? 1 : -1) : filteredData = props.filteredData.filter(element => typeof(element.scheduledDepartureTime) !== 'undefined').sort((a, b) => (a.scheduledDepartureTime > b.scheduledDepartureTime) ? 1 : -1);
+    if (props.display === 'arrival') { 
+        filteredData = props.filteredData
+        .filter(element => typeof(element.scheduledArrivalTime) !== 'undefined')
+        .sort((a, b) => (a.scheduledArrivalTime > b.scheduledArrivalTime) ? 1 : -1)
+        .map(train => ({...train, time: (train.actualArrivalTime && formatTime(train.actualArrivalTime) !== formatTime(train.scheduledArrivalTime)) ?  (<><span className="red">{formatTime(train.actualArrivalTime)}</span>
+             <span className="under">({formatTime(train.scheduledArrivalTime)})</span></>) :
+             formatTime(train.scheduledArrivalTime) }));
+     } else {
+     filteredData = props.filteredData
+        .filter(element => typeof(element.scheduledDepartureTime) !== 'undefined')
+        .sort((a, b) => (a.scheduledDepartureTime > b.scheduledDepartureTime) ? 1 : -1)
+        .map(train => ({...train, time: (train.actualDepartureTime && formatTime(train.actualDepartureTime) !== formatTime(train.scheduledDepartureTime)) ?  (<><span className="red">{formatTime(train.actualDepartureTime)}</span>
+             <span className="under">({formatTime(train.scheduledDepartureTime)})</span></>) :
+             formatTime(train.scheduledDepartureTime) }));;
+    } 
     console.log('TCL: filteredData', filteredData);
     const data = filteredData.map( train => (
     <tr key={(props.display === 'arrival') ? train.trainNumber + '_' + train.scheduledArrivalTime : train.trainNumber + '_' + train.scheduledDepartureTime}>
         <td>{train.trainNumber}</td>
         <td>{train.origin}</td>
         <td>{train.destination}</td>
-        <td>{(props.display === 'arrival') ? formatTime(train.scheduledArrivalTime) : formatTime(train.scheduledDepartureTime)}</td>
+        <td>{train.time}</td>
     </tr>));
     
     return (
-        <table className={styles.DataDisplay}>
+        <table className="DataDisplay">
             <thead>
                 <tr>
                     <th>Juna</th>
