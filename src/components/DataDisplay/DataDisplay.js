7fa028bd210fd35000348eb65402d89f7c53b213
Raphael Pinel <raphaelpinel8@gmail.com>
2019-02-13 01:27:51 +0200

    added code comments

--- src/components/DataDisplay/DataDisplay.js
+++ src/components/DataDisplay/DataDisplay.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import './DataDisplay.css';
 
-const formatTime = (dateTime) => {
+const formatTime = (dateTime) => { //returns a time in the correct timezone
     return new Intl.DateTimeFormat('fi-FI', { hour: 'numeric', minute: 'numeric'}).format(new Date(dateTime.toString()));
   }
-
 
 const DataDisplay = props => {
     let filteredData;
     if (props.display === 'arrival') { 
         filteredData = props.filteredData
-        .filter(element => typeof(element.scheduledArrivalTime) !== 'undefined')
-        .sort((a, b) => (a.scheduledArrivalTime > b.scheduledArrivalTime) ? 1 : -1)
-        .map(train => ({...train, time: (train.actualArrivalTime && formatTime(train.actualArrivalTime) !== formatTime(train.scheduledArrivalTime)) ?  (<><span className="red">{formatTime(train.actualArrivalTime)}</span>
+        .filter(element => typeof(element.scheduledArrivalTime) !== 'undefined') // filters out departures
+        .sort((a, b) => (a.scheduledArrivalTime > b.scheduledArrivalTime) ? 1 : -1) // sorting by time
+        .map(train => ({...train, time: (train.actualArrivalTime && formatTime(train.actualArrivalTime) !== formatTime(train.scheduledArrivalTime)) ?  //only if actual and scheduled arrival time differ, create special class (red) for actual time
+        (<><span className="red">{formatTime(train.actualArrivalTime)}</span>
              <span className="under">({formatTime(train.scheduledArrivalTime)})</span></>) :
              formatTime(train.scheduledArrivalTime) }));
-     } else {
+     } else { // for departures
      filteredData = props.filteredData
-        .filter(element => typeof(element.scheduledDepartureTime) !== 'undefined')
-        .sort((a, b) => (a.scheduledDepartureTime > b.scheduledDepartureTime) ? 1 : -1)
-        .map(train => ({...train, time: (train.actualDepartureTime && formatTime(train.actualDepartureTime) !== formatTime(train.scheduledDepartureTime)) ?  (<><span className="red">{formatTime(train.actualDepartureTime)}</span>
+        .filter(element => typeof(element.scheduledDepartureTime) !== 'undefined') // filters out arrivals
+        .sort((a, b) => (a.scheduledDepartureTime > b.scheduledDepartureTime) ? 1 : -1) // sorting by time
+        .map(train => ({...train, time: (train.actualDepartureTime && formatTime(train.actualDepartureTime) !== formatTime(train.scheduledDepartureTime)) ?  //only if actual and scheduled departure time differ, special class (red) for actual time
+        (<><span className="red">{formatTime(train.actualDepartureTime)}</span>
              <span className="under">({formatTime(train.scheduledDepartureTime)})</span></>) :
              formatTime(train.scheduledDepartureTime) }));;
     } 
-    const data = filteredData.map( train => (
-    <tr key={(props.display === 'arrival') ? train.trainNumber + '_' + train.scheduledArrivalTime : train.trainNumber + '_' + train.scheduledDepartureTime} className={(train.cancelled) ? 'cancelled' : null}>
-        <td>{train.trainNumber}</td>
-        <td>{train.origin}</td>
-        <td>{train.destination}</td>
-    <td>{train.time} {(train.cancelled) ? <span className="cancelled">Cancelled</span> : null}</td>
+    const finalData = filteredData.map( train => (
+    <tr 
+        key={(props.display === 'arrival') ? train.trainNumber + '_' + train.scheduledArrivalTime 
+                                            : train.trainNumber + '_' + train.scheduledDepartureTime} // added timestamps to avoid duplicate keys
+        className={(train.cancelled) ? 'cancelled' : null}>
+            <td>{train.trainNumber}</td>
+            <td>{train.origin}</td>
+            <td>{train.destination}</td>
+            <td>{train.time} {(train.cancelled) ? <span className="cancelled">Cancelled</span> : null}</td>
     </tr>));
     
     return (
@@ -42,7 +46,7 @@ const DataDisplay = props => {
                 </tr>
             </thead>
             <tbody>
-            {data}
+            {finalData}
             </tbody>
         </table>
     );
