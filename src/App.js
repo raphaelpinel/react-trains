2d2cfb69283263a69cc3b46fb6d74f156dfb34e0
Raphael Pinel <raphaelpinel8@gmail.com>
2019-02-12 18:41:59 +0200

    fixed an error by fetching the right endpoint

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
       isLoading:true,
       stations: [],
       passengerStations: [],
+      todaysTrains: [],
       selectedStation: null,
       filteredData: [],
       tabIndex: 0
@@ -25,14 +26,16 @@ class App extends Component {
     this.setState({
       selectedStation
     });
-    if (selectedStation) {
+    if (selectedStation && this.state.isLoading === false) {
       this.fetchAndFilterData(selectedStation);
+      //this.filterData2(selectedStation);
     }
   }
 
   componentDidMount() {
-    this.setState({ isLoading: true })
+    this.setState({ isLoading: true });
     this.fetchStations();
+    this.fetchTodaysTrains();
   }
 
   fetchStations() {
@@ -48,62 +51,94 @@ class App extends Component {
     .catch(error => console.log('parsing failed', error)
     );
   }
+
+  fetchTodaysTrains() {
+    this.setState({ isLoading: true});
+    const dateNow = new Date().toISOString().slice(0, 10);
+    fetch(`${API}trains/${dateNow}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log('TCL: App -> fetchTodaysTrains -> data', data);
+      this.setState({todaysTrains: data, isLoading: true})
+    })
+    .catch(error => console.log(error));
+  }
+
+  filterData2(selectedStation) {
+    const data = [];//this.state.todaysTrains;
+    // const filteredData = data.filter(train => train.timeTableRows.filter(timeEntry => timeEntry.stationShortCode === selectedStation.value));
+    const filteredData = data.map(train => train.filter(train.timeTableRows.filter(timeEntry => timeEntry.stationShortCode === selectedStation.value)));
+		console.log('TCL: App -> filteredData', filteredData);
+    
+  }
   
   formatTime(dateTime) {
     return new Intl.DateTimeFormat('fi-FI', { hour: 'numeric', minute: 'numeric'}).format(new Date(dateTime.toString()));
   }
 
-  fetchAndFilterData(selectedStation) {  
-    fetch(`${API}live-trains/station/${selectedStation.value}`)
-    .then(response => response.json())
-    .then(data => {
-    console.log('TCL: fetchAndFilterData -> data', data);
+  fetchAndFilterData(selectedStation) { 
+    const dateTimeNow = new Date().toJSON(); 
+    const data = this.state.todaysTrains;
+    //fetch(`${API}live-trains/station/${selectedStation.value}`)
+    //.then(response => response.json())
+    //.then(data => {
+    //console.log('TCL: fetchAndFilterData -> data', data);
     const filteredData = data.map((train => {
       const trainNumber = train.trainType + ' ' + train.trainNumber;
       const originShortCode = train.timeTableRows[0].stationShortCode;
       const origin = this.state.stations.find(station => station.value === originShortCode).label;
       const destinationShortCode = train.timeTableRows[train.timeTableRows.length - 1]['stationShortCode'];
       const destination = this.state.stations.find(station => station.value === destinationShortCode).label;
-      let scheduledArrivalTime;
+      
+      let scheduledArrivalTime; // arrivals
       let actualArrivalTime;
       const arrivalTimeTable = {...train.timeTableRows.filter(element => element.stationShortCode === selectedStation.value && element.type === 'ARRIVAL')[0]};
       if(arrivalTimeTable) {
         if (arrivalTimeTable.hasOwnProperty('scheduledTime')) {
-          scheduledArrivalTime = this.formatTime(arrivalTimeTable.scheduledTime);  
+          scheduledArrivalTime = arrivalTimeTable.scheduledTime;  
         }
         if(arrivalTimeTable.hasOwnProperty('actualTime')) {
-         actualArrivalTime =  this.formatTime(arrivalTimeTable.actualTime);  
+         actualArrivalTime =  arrivalTimeTable.actualTime;  
         } else if (arrivalTimeTable.hasOwnProperty('liveEstimateTime')) {
-         actualArrivalTime = this.formatTime(arrivalTimeTable.liveEstimateTime);
+         actualArrivalTime = arrivalTimeTable.liveEstimateTime;
         } else {
          actualArrivalTime = false;
         }
       } 
-      let scheduledDepartureTime;
+      let scheduledDepartureTime; // departures
       let actualDepartureTime;
       const departureTimeTable = {...train.timeTableRows.filter(element => element.stationShortCode === selectedStation.value && element.type === 'DEPARTURE')[0]}
-      
       if(departureTimeTable) {
         if (departureTimeTable.hasOwnProperty('scheduledTime')) {
-          scheduledDepartureTime = this.formatTime(departureTimeTable.scheduledTime);
+          scheduledDepartureTime = departureTimeTable.scheduledTime;
         }
         if(departureTimeTable.hasOwnProperty('actualTime')) {
-         actualDepartureTime =  this.formatTime(departureTimeTable.actualTime);
+         actualDepartureTime =  departureTimeTable.actualTime;
         } else if (departureTimeTable.hasOwnProperty('liveEstimateTime')) {
-         actualDepartureTime = this.formatTime(departureTimeTable.liveEstimateTime);
+         actualDepartureTime = departureTimeTable.liveEstimateTime;
         } else {
          actualDepartureTime = false;
         }
       }
-			
+
       return {...train, trainNumber, origin, destination, scheduledArrivalTime, actualArrivalTime,  scheduledDepartureTime, actualDepartureTime};
-    }));  
+    }))
+    .filter(train => train.trainCategory !== 'Cargo')
+    .filter(train => train.actualArrivalTime > dateTimeNow || train.scheduledArrivalTime > dateTimeNow || train.actualDepartureTime > dateTimeNow || train.scheduledDepartureTime > dateTimeNow)
+    ; 
+    
+    
+    console.log('TCL: App -> fetchAndFilterData -> dateTimeNow', dateTimeNow);
+
+    
+     
     this.setState({filteredData});
 		console.log('TCL: App -> fetchAndFilterData -> filteredData', filteredData)
-    })
-    .catch(error => console.log('parsing failed', error)
-    );
-  }
+    }
+    //)
+    //.catch(error => console.log('parsing failed', error)
+    //);
+  //}
 
   render() {  
     return (
--- src/components/DataDisplay/DataDisplay.js
+++ src/components/DataDisplay/DataDisplay.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import styles from './DataDisplay.module.css'
+import styles from './DataDisplay.module.css';
+
+const formatTime = (dateTime) => {
+    return new Intl.DateTimeFormat('fi-FI', { hour: 'numeric', minute: 'numeric'}).format(new Date(dateTime.toString()));
+  }
+
 
 const DataDisplay = props => {
     let filteredData;
-    props.display === 'arrival' ? filteredData = props.filteredData.filter(element => typeof(element.scheduledArrivalTime) !== 'undefined') : filteredData = props.filteredData.filter(element => typeof(element.scheduledDepartureTime) !== 'undefined');
+    props.display === 'arrival' ? filteredData = props.filteredData.filter(element => typeof(element.scheduledArrivalTime) !== 'undefined').sort((a, b) => (a.scheduledArrivalTime > b.scheduledArrivalTime) ? 1 : -1) : filteredData = props.filteredData.filter(element => typeof(element.scheduledDepartureTime) !== 'undefined').sort((a, b) => (a.scheduledDepartureTime > b.scheduledDepartureTime) ? 1 : -1);
     console.log('TCL: filteredData', filteredData);
     const data = filteredData.map( train => (
     <tr key={train.trainNumber}>
         <td>{train.trainNumber}</td>
         <td>{train.origin}</td>
         <td>{train.destination}</td>
-        <td>{(props.display === 'arrival') ? train.scheduledArrivalTime : train.scheduledDepartureTime}</td>
+        <td>{(props.display === 'arrival') ? formatTime(train.scheduledArrivalTime) : formatTime(train.scheduledDepartureTime)}</td>
     </tr>));
     
     return (
@@ -20,7 +25,7 @@ const DataDisplay = props => {
                     <th>Juna</th>
                     <th>Lähtöasema</th>
                     <th>Pääteasema</th>
-                    <th>Saapuu</th> 
+                    <th>{(props.display === 'arrival') ? 'Saapuu' : 'Lähtee'}</th> 
                 </tr>
             </thead>
             <tbody>
