b7b547e16e54e60e83cce9ef68549f16122acb69
Raphael Pinel <raphaelpinel8@gmail.com>
2019-02-15 07:57:09 +0200

    temporarily removed translation from WithLoading higher order component (which was not working and need extra module)

--- src/hoc/withLoading.js
+++ src/hoc/withLoading.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+//import { useTranslation } from 'react-i18next';
 
 const WithLoading = Component => {
-    const { t, i18n } = useTranslation();
+    //const { t, i18n } = useTranslation();
     return ({ isLoaded, ...props }) => {
         if (isLoaded) { return (<Component {...props} />) }
-        return (<p>{t('Loading')}...</p>);
+        return (<p>Loading...</p>);
     }
 }
 
