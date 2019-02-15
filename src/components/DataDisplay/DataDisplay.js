77ef05b9f61c951470f042aa38f8ccf47962b4ef
Raphael Pinel <raphaelpinel8@gmail.com>
2019-02-15 19:39:54 +0200

    changed to a time format with colon (:) to fit both languages

--- src/components/DataDisplay/DataDisplay.js
+++ src/components/DataDisplay/DataDisplay.js
@@ -3,11 +3,12 @@ import { useTranslation } from 'react-i18next';
 import './DataDisplay.css';
 
 const formatTime = (dateTime) => { //returns a time in the correct timezone
-    return new Intl.DateTimeFormat('fi-FI', { hour: 'numeric', minute: 'numeric'}).format(new Date(dateTime.toString()));
+    return new Intl.DateTimeFormat('default', { hour: 'numeric', minute: 'numeric'}).format(new Date(dateTime.toString()));
   }
 
 const DataDisplay = props => {
-    const {t} = useTranslation();
+    const {t, i18n} = useTranslation();
+    const language = i18n.language;
     const filteredData = props.filteredData
         .sort((a, b) => (a.scheduledTime > b.scheduledTime) ? 1 : -1) // sorting by time
         .map(train => ({...train, time: (train.actualTime && formatTime(train.actualTime) !== formatTime(train.scheduledTime)) ?  //only if actual and scheduled arrival time differ, create special class (red) for actual time
